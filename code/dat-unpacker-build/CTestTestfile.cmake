d5f9432996354690c804d30000915e8aecd0a6f0
Otakar Jašek <jasekota@fel.cvut.cz>
2018-03-29 10:16:08 +0200

    No build dir

--- code/dat-unpacker-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/otaj/files/dip/master-thesis/code/dat-unpacker
-# Build directory: /home/otaj/files/dip/master-thesis/code/dat-unpacker-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
