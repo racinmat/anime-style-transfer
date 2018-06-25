fixed normalization and denormalization

code/mod-cycle-gan/cycle/models/anime/_utils.py
@@ -1,20 +1,18 @@
 import tensorflow as tf
-import cycle
-
-MAX_RAYDIST = 131.0
 
 
 def normer(data, name='normer'):
-    tmp_data = data[:, :, 0] / MAX_RAYDIST
-    ndata = tf.concat((tmp_data[:, :, None], data[:, :, 1:]), axis=-1)
-    ndata = (ndata * 2) - 1
+    # input images are in range 0 to 255
+    # output images are in range -1 to 1
+    tmp_data = data / 255.
+    ndata = (tmp_data * 2) - 1
     return tf.identity(ndata, name=name)
 
 
 def denormer(data, name='denormer'):
     ndata = (data + 1) / 2
-    tmp_data = ndata[:, :, 0] * MAX_RAYDIST
-    return tf.concat((tmp_data[:, :, None], ndata[:, :, 1:]), axis=-1, name=name)
+    tmp_data = ndata * 255
+    return tf.identity(tmp_data, name=name)
 
 
 def _conv_visual(batch):

code/mod-cycle-gan/data_preparation/images_to_tfrecord.py
@@ -11,6 +11,9 @@ from scipy.misc import imresize
 
 
 def process_sample(data):
+    print('min is: ', imresize(data, (512, 512)).min())
+    print('max is: ', imresize(data, (512, 512)).max())
+    exit()
     return imresize(data, (512, 512))
 
 
@@ -32,9 +35,6 @@ def run(infiles, outfile, tf_name):
         pbar.update(i)
         try:
             data = np.array(Image.open(f))
-            # if i % 10 == 0:
-            #     logging.info('Opening %s', f)
-            #     logging.info('%d / %d : \t %s', i, len(infiles), f)
             ex = tf.train.Example(features=tf.train.Features(
                 feature={name: tf.train.Feature(float_list=tf.train.FloatList(value=process_sample(data).ravel()))}))
             writer.write(ex.SerializeToString())
@@ -75,7 +75,7 @@ def run_real():
     # infiles = get_real_images_cityscapes()
     # tfrecord_name = 'cityscapes.tfrecord'
     infiles = get_real_images_ade20k()  # hopefully ade20k will be more representative than cityscapes
-    tfrecord_name = 'ade20k.tfrecord'
+    tfrecord_name = 'ade20ks.tfrecord'
     print('{} files to process'.format(len(infiles)))
     run(infiles, osp.join(tfrecords_root, tfrecord_name), 'real')
 