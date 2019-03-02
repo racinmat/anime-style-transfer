93e6a197ebf80ba2c98c421067a3233956005e52
Matěj Račinský <racinsky.matej@seznam.cz>
2019-03-02 03:20:34 +0100

    prepared hopefully good architecture

--- code/neural_style_transfer/anime_dimension_reduction_keras.py
+++ code/neural_style_transfer/anime_dimension_reduction_keras.py
@@ -2,7 +2,8 @@ import numpy as np
 import tensorflow as tf
 from keras.losses import mean_squared_error
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input, Flatten, Reshape, Convolution2D, Convolution2DTranspose, Conv2D, Conv2DTranspose
+from keras.layers import Dense, Input, Flatten, Reshape, Convolution2D, Convolution2DTranspose, Conv2D, Conv2DTranspose, \
+    ZeroPadding2D
 from keras.optimizers import Adam
 from keras import backend as K
 from keras.callbacks import TensorBoard
@@ -55,23 +56,23 @@ def main(_):
     validation_data = (np.array([i[0] for i in validation_data]).reshape(-1, 432, 768, 3),
                        np.array([i[1] for i in validation_data]).reshape(-1, 432, 768, 3))
 
-    z_size = 2
+    z_size = 20
     regul_const = 10e-7
     input_tensor = Input(shape=(432, 768, 3))
-    out = Conv2D(8, (3, 3), activation='elu', border_mode='valid')(input_tensor)
-    out = Conv2D(16, (3, 3), activation='elu', border_mode='valid')(out)
-    out = Conv2D(32, (3, 3), activation='elu', border_mode='valid', name='bottleneck')(out)
-    # m.add(Flatten(input_shape=(432, 768, 3)))
-    # m.add(Dense(512, activation='elu'))
-    # m.add(Dense(128, activation='elu'))
-    # m.add(Dense(z_size, activation='linear', name='bottleneck', activity_regularizer=l1(regul_const)))
-    # m.add(Dense(128, activation='elu'))
-    # m.add(Dense(512, activation='elu'))
-    # m.add(Reshape((432, 768, 3), name='decoder'))
-    out = Conv2DTranspose(32, (3, 3), activation='elu', padding='valid')(out)
-    out = Conv2DTranspose(16, (3, 3), activation='elu', padding='valid')(out)
-    out = Conv2DTranspose(8, (3, 3), activation='elu', padding='valid')(out)
-    out = Conv2D(3, (3, 3), activation='elu', padding='same')(out)
+    out = Conv2D(16, kernel_size=(3, 3), strides=(3, 3), activation='elu', border_mode='valid')(input_tensor)
+    out = Conv2D(32, kernel_size=(3, 3), strides=(3, 3), activation='elu', border_mode='valid')(out)
+    out = Conv2D(64, kernel_size=(3, 3), strides=(3, 3), activation='elu', border_mode='valid')(out)
+    out = Conv2D(128, kernel_size=(3, 3), strides=(3, 3), activation='elu', border_mode='valid')(out)
+    out = Flatten()(out)
+    out = Dense(z_size, activation='linear', name='bottleneck', activity_regularizer=l1(regul_const))(out)
+    out = Dense(8 * 14 * 64, activation='elu')(out)
+    out = Reshape((8, 14, 64))(out)
+    out = Conv2DTranspose(128, kernel_size=(2, 2), strides=(2, 2), activation='elu', padding='valid')(out)
+    out = Conv2DTranspose(64, kernel_size=(3, 3), strides=(3, 3), activation='elu', padding='valid')(out)
+    out = Conv2DTranspose(32, kernel_size=(3, 3), strides=(3, 3), activation='elu', padding='valid')(out)
+    out = ZeroPadding2D(padding=(0, 2))(out)
+    out = Conv2DTranspose(16, kernel_size=(3, 3), strides=(3, 3), activation='elu', padding='valid')(out)
+    out = Conv2DTranspose(3, kernel_size=(1, 1), activation='elu', padding='same')(out)
     # m.add(Dense(432*768*3, activation='linear'))
     m = Model(inputs=input_tensor, outputs=out)
     m.compile(loss=mean_squared_error, optimizer=Adam())
@@ -85,19 +86,25 @@ def main(_):
     config.gpu_options.allow_growth = True
     set_session(tf.Session(config=config))
 
-    history = m.fit_generator(data_gen, steps_per_epoch=100, epochs=50, verbose=1,
+    history = m.fit_generator(data_gen, steps_per_epoch=200, epochs=100, verbose=1,
                               validation_data=validation_data, validation_steps=validation_batches * batch_size,
                               callbacks=[tensorboard])
 
+    model_json = m.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    m.save_weights("model.h5")
+
     encoder = Model(m.input, m.get_layer('bottleneck').output)
     decoder = extract_decoder(m)
-    z_pca = encoder.predict(data)  # bottleneck representation
-    r_pca = denormer(m.predict(data))
+    z_pca = encoder.predict(validation_data)  # bottleneck representation
+    r_pca = denormer(m.predict(validation_data))
 
     show_factors(decoder, m.get_layer('bottleneck').units, 'anime', (768, 432))
 
-    # visualize_data(x_train, y_train, r_pca_train, z_pca_train, 'train_anime' + suffix)
-    # visualize_data(x_test, y_test, r_pca_test, z_pca_test, 'test_anime' + suffix)
+    visualize_data(x_train, y_train, r_pca_train, z_pca_train, 'train_anime' + suffix)
+    visualize_data(x_test, y_test, r_pca_test, z_pca_test, 'test_anime' + suffix)
 
     plot_network_history(history, 'anime')
 
