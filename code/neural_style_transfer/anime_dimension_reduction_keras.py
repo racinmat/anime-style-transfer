97cbaaa586d9d1e60abbe4f0d3c4071414ad05a1
Matěj Račinský <racinsky.matej@seznam.cz>
2019-03-02 02:19:29 +0100

    finally working trainig, somehow

--- code/neural_style_transfer/anime_dimension_reduction_keras.py
+++ code/neural_style_transfer/anime_dimension_reduction_keras.py
@@ -28,24 +28,31 @@ def create_dataset(tf_record, batch_size):
     data = tf.data.TFRecordDataset(tf_record)
     data = data.map(TFReader._parse_example_encoded, num_parallel_calls=8)
     data = data.map(norm_and_resize, num_parallel_calls=8)
-    # data = tf.data.Dataset.zip((data, data))
+    data = tf.data.Dataset.zip((data, data))
     data = data.apply(tf.data.experimental.shuffle_and_repeat(buffer_size=100))
     data = data.batch(batch_size, drop_remainder=True)
-    data = data.prefetch(4)
+    data = data.prefetch(batch_size * 5)
     return data
 
 
+def tf_data_generator(iterator):
+    next_batch = iterator.get_next()
+    while True:
+        yield K.get_session().run(next_batch)
+
+
 def main(_):
-    batch_size = 8  # todo: check and try bigger
+    batch_size = 16
     data = create_dataset('../../datasets/anime/no-game-no-life-ep-2.tfrecord', batch_size)
     iterator = data.make_one_shot_iterator()
+    data_gen = tf_data_generator(iterator)
 
     K.set_image_data_format('channels_last')  # set format
 
     z_size = 2
     regul_const = 10e-7
-    input_tensor = Input(tensor=iterator.get_next())
-    out = Conv2D(8, (3, 3), activation='elu', border_mode='valid', batch_input_shape=(batch_size, 432, 768, 3))(input_tensor)
+    input_tensor = Input(shape=(432, 768, 3))
+    out = Conv2D(8, (3, 3), activation='elu', border_mode='valid')(input_tensor)
     out = Conv2D(16, (3, 3), activation='elu', border_mode='valid')(out)
     out = Conv2D(32, (3, 3), activation='elu', border_mode='valid', name='bottleneck')(out)
     # m.add(Flatten(input_shape=(432, 768, 3)))
@@ -61,22 +68,28 @@ def main(_):
     out = Conv2D(3, (3, 3), activation='elu', padding='same')(out)
     # m.add(Dense(432*768*3, activation='linear'))
     m = Model(inputs=input_tensor, outputs=out)
-    m.compile(loss=mean_squared_error, optimizer=Adam(), target_tensors=iterator.get_next())
+    m.compile(loss=mean_squared_error, optimizer=Adam())
     print(m.summary())
     tensorboard = TensorBoard(
-        log_dir='logs/anime', histogram_freq=5, embeddings_freq=5, embeddings_layer_names=['bottleneck'],
-        write_images=True, embeddings_data=iterator.get_next(), embeddings_metadata='embeddings.tsv')
+        log_dir='logs/anime', histogram_freq=5, write_images=True,
+        # embeddings_freq=5, embeddings_layer_names=['bottleneck'],
+        # embeddings_data=tf_data_generator(iterator), embeddings_metadata='embeddings.tsv'
+    )
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     set_session(tf.Session(config=config))
 
-    history = m.fit(steps_per_epoch=100, epochs=50, verbose=1,
-                    # validation_split=0.2,
-                    # validation_data=(1, 1),
-                    validation_data=(iterator.get_next(), iterator.get_next()),
-                    validation_steps=4,
-                    callbacks=[tensorboard]
-                    )
+    # fuck it, I must create some validation data and keep it in memory, because fuck you
+    validation_batches = 2
+    validation_data = [next(data_gen) for i in range(validation_batches)]
+    validation_data = (np.array([i[0] for i in validation_data]).reshape(-1, 432, 768, 3),
+                       np.array([i[1] for i in validation_data]).reshape(-1, 432, 768, 3))
+
+    history = m.fit_generator(data_gen, steps_per_epoch=100, epochs=50, verbose=1,
+                              validation_data=validation_data,
+                              validation_steps=validation_batches * batch_size,
+                              callbacks=[tensorboard]
+                              )
 
     encoder = Model(m.input, m.get_layer('bottleneck').output)
     decoder = extract_decoder(m)
